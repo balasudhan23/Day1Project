52abb4eccc01144f9855b605cd29090673422d41
balasudhan23 <balasudhan2000@gmail.com>
2023-03-23 22:18:38 +0530

    Task1

---
+++ src/main/java/org/baseclass/Bala_BaseClass.java
@@ -0,0 +1,21 @@
+package org.baseclass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Bala_BaseClass {
+public static WebDriver driver;
+	
+	
+	public static WebDriver chromeLaunch() {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		driver =new ChromeDriver(options);
+		return driver;
+	}
+
+}
---
+++ src/test/java/org/test/Task1.java
@@ -0,0 +1,10 @@
+package org.test;
+
+import org.baseclass.Bala_BaseClass;
+
+public class Task1 extends Bala_BaseClass {
+	public static void main(String[] args) {
+		chromeLaunch();
+	}
+
+}
